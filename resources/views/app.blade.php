b2931ba05ea3e2552cca948d49fcd8b67618f143
Felipe Martins <you@example.com>
2021-01-25 23:19:52 -0300

    changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,12 +25,12 @@
             window.OneSignal = window.OneSignal || [];
             OneSignal.push(function() {
                 OneSignal.init({
-                appId: "1e2169ae-d90c-4520-bb55-18451f7bf630",
-                notifyButton: {
-                    enable: false,
-                },
+                    appId: "1e2169ae-d90c-4520-bb55-18451f7bf630",
+                    autoRegister: true,
+                    notifyButton: {
+                        enable: false,
+                    },
                 });
-                OneSignal.showNativePrompt();
             });
         </script>
     </head>
